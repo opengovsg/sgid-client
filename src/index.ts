f89da9b3475c9be4cc0939585666f373bd2776fd
LoneRifle <LoneRifle@users.noreply.github.com>
2021-05-31 10:49:25 +0800

    refactor: use node-oidc-client
    Switch out handwritten code in favour of battle-tested npm package

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,17 @@
-import axios from 'axios'
 import fs from 'fs'
 import jwt from 'jsonwebtoken'
 import jwtDecode from 'jwt-decode'
 import { JWE, JWK } from 'node-jose'
+import { Client, generators, Issuer } from 'openid-client'
 
 const isKey = (key: string) => key.startsWith('-----BEGIN')
 const RS256 = 'RS256'
 
 export class SgidClient {
-  private endpoint: string
-  private clientId: string
-  private clientSecret: string
   private privateKey: string | Buffer
   private publicKey: string | Buffer
-  private redirectUri: string
+
+  private sgID: Client
 
   constructor({
     endpoint,
@@ -30,18 +28,33 @@ export class SgidClient {
     publicKey: string
     redirectUri: string
   }) {
-    this.endpoint = endpoint
-    this.clientId = clientId
-    this.clientSecret = clientSecret
     this.privateKey = isKey(privateKey)
       ? privateKey
       : fs.readFileSync(privateKey)
     this.publicKey = isKey(publicKey) ? publicKey : fs.readFileSync(publicKey)
-    this.redirectUri = redirectUri
+
+    // TODO: Discover sgID issuer metadata via .well-known endpoint
+    const issuer = new Issuer({
+      issuer: 'sgID',
+      authorization_endpoint: `${endpoint}/authorize`,
+      token_endpoint: `${endpoint}/token`,
+      userinfo_endpoint: `${endpoint}/userinfo`,
+    })
+
+    this.sgID = new issuer.Client({
+      client_id: clientId,
+      client_secret: clientSecret,
+      redirect_uris: [redirectUri],
+    })
   }
 
   authorizationUrl(state: string): string {
-    return `${this.endpoint}/authorize?redirect_uri=${this.redirectUri}&client_id=${this.clientId}&state=${state}&response_type=code&scope=myinfo.nric_number%20openid&nonce=randomnonce`
+    const nonce = generators.nonce()
+    return this.sgID.authorizationUrl({
+      scope: 'myinfo.nric_number openid',
+      nonce,
+      state,
+    })
   }
 
   decodeIdToken(token: string): string {
@@ -52,27 +65,11 @@ export class SgidClient {
   }
 
   async callback(code: string): Promise<{ sub: string; accessToken: string }> {
-    return axios
-      .post<{ access_token: string; id_token: string }>(
-        `${this.endpoint}/token`,
-        {
-          client_id: this.clientId,
-          client_secret: this.clientSecret,
-          grant_type: 'authorization_code',
-          redirect_uri: this.redirectUri,
-          code,
-        },
-        {
-          headers: {
-            'Cache-Control': 'no-cache',
-            'Content-Type': 'application/json',
-            Accept: 'application/json',
-          },
-        },
-      )
-      .then((response) => response.data)
-      .then(({ access_token: accessToken, id_token: idToken }) => {
-        const sub = this.decodeIdToken(idToken)
+    return this.sgID
+      .callback(undefined, { code })
+      .then(({ access_token, id_token: idToken }) => {
+        const sub = this.decodeIdToken(`${idToken}`)
+        const accessToken = `${access_token}`
         return { sub, accessToken }
       })
   }
@@ -80,17 +77,10 @@ export class SgidClient {
   async userinfo(
     accessToken: string,
   ): Promise<{ sub: string; data: Record<string, string> }> {
-    return axios
-      .get<{ sub: string; key: string; data: Record<string, string> }>(
-        `${this.endpoint}/userinfo`,
-        {
-          headers: {
-            Accept: 'application/json',
-            Authorization: `Bearer ${accessToken}`,
-          },
-        },
+    return this.sgID
+      .userinfo<{ sub: string; key: string; data: Record<string, string> }>(
+        accessToken,
       )
-      .then((response) => response.data)
       .then(async ({ sub, key: encryptedPayloadKey, data }) => {
         const privateKey = await JWK.asKey(this.privateKey, 'pem')
         const { plaintext: payloadKey } = await JWE.createDecrypt(
