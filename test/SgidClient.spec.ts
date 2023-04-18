24de4795b48d4f063c210e81f27a81a9551465e4
raynerljm <raynerljm@gmail.com>
2023-04-18 20:06:25 +0800

    style: prefix codeVerifier and codeChallenge functions with verbs

--- test/SgidClient.spec.ts
+++ test/SgidClient.spec.ts
@@ -331,9 +331,9 @@ describe('SgidClient', () => {
     })
   })
 
-  describe('codeVerifier', () => {
+  describe('generateCodeVerifier', () => {
     it('should generate a code verifier of length 43 when no length is provided', () => {
-      const codeVerifier = client.codeVerifier()
+      const codeVerifier = client.generateCodeVerifier()
 
       expect(codeVerifier.length).toBe(43)
       expect(codeVerifier).toMatch(codeVerifierAndChallengePattern)
@@ -341,7 +341,7 @@ describe('SgidClient', () => {
 
     it('should generate a code verifier of specified length when length between 43 (inclusive) and 128 (inclusive) is provided', () => {
       for (let length = 43; length <= 128; length++) {
-        const codeVerifier = client.codeVerifier(length)
+        const codeVerifier = client.generateCodeVerifier(length)
         expect(codeVerifier.length).toBe(length)
         expect(codeVerifier).toMatch(codeVerifierAndChallengePattern)
       }
@@ -349,18 +349,18 @@ describe('SgidClient', () => {
 
     it('should throw an error when a length < 43 or length > 128 is provided', () => {
       for (const length of [-1, 0, 42, 129, 138, 999]) {
-        expect(() => client.codeVerifier(length)).toThrowError(
+        expect(() => client.generateCodeVerifier(length)).toThrowError(
           `The code verifier should have a minimum length of 43 and a maximum length of 128. Length of ${length} was provided`,
         )
       }
     })
   })
 
-  describe('codeChallenge', () => {
+  describe('generateCodeChallenge', () => {
     it('should match the specified pattern', () => {
       const mockCodeVerifier = 'bbGcObXZC1YGBQZZtZGQH9jsyO1vypqCGqnSU_4TI5S'
 
-      expect(client.codeChallenge(mockCodeVerifier)).toMatch(
+      expect(client.generateCodeChallenge(mockCodeVerifier)).toMatch(
         codeVerifierAndChallengePattern,
       )
     })
@@ -368,8 +368,8 @@ describe('SgidClient', () => {
     it('should be deterministic (return the same code challenge given the same code verifier)', () => {
       const mockCodeVerifier = 'bbGcObXZC1YGBQZZtZGQH9jsyO1vypqCGqnSU_4TI5S'
 
-      const firstCodeChallenge = client.codeChallenge(mockCodeVerifier)
-      const secondCodeChallenge = client.codeChallenge(mockCodeVerifier)
+      const firstCodeChallenge = client.generateCodeChallenge(mockCodeVerifier)
+      const secondCodeChallenge = client.generateCodeChallenge(mockCodeVerifier)
 
       expect(firstCodeChallenge).toBe(secondCodeChallenge)
     })
