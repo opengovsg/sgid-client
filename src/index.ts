242f68a7c77d96118b38049dd707831ddb426c70
raynerljm <raynerljm@gmail.com>
2023-04-19 16:33:09 +0800

    feat: create new sdk for v2

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,6 @@ import * as Errors from './error'
 import { convertPkcs1ToPkcs8 } from './util'
 
 const SGID_SIGNING_ALG = 'RS256'
-export const DEFAULT_SGID_CODE_CHALLENGE_METHOD = 'S256'
 export const DEFAULT_SCOPE = 'myinfo.nric_number openid'
 const SGID_SUPPORTED_FLOWS: ResponseType[] = ['code']
 const SGID_AUTH_METHOD: ClientAuthMethod = 'client_secret_post'
@@ -28,8 +27,8 @@ export type SgidClientParams = {
 }
 
 export class SgidClient {
-  private apiVersion: number
   private privateKey: string
+
   private sgID: Client
 
   /**
@@ -53,7 +52,6 @@ export class SgidClient {
     hostname = 'https://api.id.gov.sg',
     apiVersion = 1,
   }: SgidClientParams) {
-    this.apiVersion = apiVersion
     // TODO: Discover sgID issuer metadata via .well-known endpoint
     const { Client } = new Issuer({
       issuer: new URL(hostname).origin,
@@ -83,7 +81,6 @@ export class SgidClient {
   }
 
   /**
-<<<<<<< HEAD
    * Generates authorization url to redirect end-user to sgID login page.
    * @param state A string which will be passed back to your application once the end-user
    * logs in. You should use this to prevent cross-site request forgery attacks (see
@@ -95,100 +92,18 @@ export class SgidClient {
    * and returned. To prevent this behaviour, specify null for this param.
    * @param redirectUri The redirect URI used in the authorization request. Defaults to the one
    * passed to the SgidClient constructor.
-=======
-   * Generates authorization url for sgID OIDC flow
-   * @param state A random string to prevent CSRF
-   * @param scopes Array or space-separated scopes, must include openid
-   * @param nonce Specify null if no nonce
-   * @param redirectUri The
-   * @param codeChallenge The code challenge from the code verifier used for PKCE enhancement
-   * @param codeChallengeMethod The code challenge method used to generate the code challenge from the code verifier, must be `S256` redirect URI used in the authorization request, defaults to the one registered with the client
-   * @returns
->>>>>>> 3f3e979 (docs: add jdocs for codeChallenge and codeChallengeMethod)
    */
   authorizationUrl(
     state: string,
     scope: string | string[] = DEFAULT_SCOPE,
     nonce: string | null = generators.nonce(),
     redirectUri: string = this.getFirstRedirectUri(),
-    codeChallenge?: string,
-    codeChallengeMethod: 'plain' | 'S256' = DEFAULT_SGID_CODE_CHALLENGE_METHOD,
   ): { url: string; nonce?: string } {
-    switch (this.apiVersion) {
-      case 1:
-        return this.authorizationUrlV1({ state, scope, nonce, redirectUri })
-
-      case 2:
-        return this.authorizationUrlV2({
-          state,
-          scope,
-          nonce,
-          redirectUri,
-          codeChallenge,
-          codeChallengeMethod,
-        })
-
-      default:
-        // eslint-disable-next-line typesafe/no-throw-sync-func
-        throw new Error(
-          `ApiVersion ${this.apiVersion} provided is invalid for function 'authorizationUrl'`,
-        )
-    }
-  }
-
-  private authorizationUrlV1({
-    state,
-    scope,
-    nonce,
-    redirectUri,
-  }: {
-    state: string
-    scope: string | string[]
-    nonce: string | null
-    redirectUri: string
-  }): { url: string; nonce?: string } {
     const url = this.sgID.authorizationUrl({
       scope: typeof scope === 'string' ? scope : scope.join(' '),
       nonce: nonce ?? undefined,
       state,
       redirect_uri: redirectUri,
-    })
-    const result: { url: string; nonce?: string } = { url }
-    if (nonce) {
-      result.nonce = nonce
-    }
-    return result
-  }
-
-  private authorizationUrlV2({
-    state,
-    scope,
-    nonce,
-    redirectUri,
-    codeChallenge,
-    codeChallengeMethod,
-  }: {
-    state: string
-    scope: string | string[]
-    nonce: string | null
-    redirectUri: string
-    codeChallenge?: string
-    codeChallengeMethod: 'plain' | 'S256'
-  }): { url: string; nonce?: string } {
-    if (codeChallenge === undefined) {
-      // eslint-disable-next-line typesafe/no-throw-sync-func
-      throw new Error(
-        "Code challenge must be provided in 'authorizationUrl' when using apiVersion 2",
-      )
-    }
-
-    const url = this.sgID.authorizationUrl({
-      scope: typeof scope === 'string' ? scope : scope.join(' '),
-      nonce: nonce ?? undefined,
-      state,
-      redirect_uri: redirectUri,
-      code_challenge: codeChallenge,
-      code_challenge_method: codeChallengeMethod,
     })
     const result: { url: string; nonce?: string } = { url }
     if (nonce) {
@@ -222,26 +137,6 @@ export class SgidClient {
     code: string,
     nonce: string | null = null,
     redirectUri = this.getFirstRedirectUri(),
-    codeVerifier?: string,
-  ): Promise<{ sub: string; accessToken: string }> {
-    switch (this.apiVersion) {
-      case 1:
-        return this.callbackV1(code, nonce, redirectUri)
-      case 2:
-        return this.callbackV2(code, nonce, redirectUri, codeVerifier)
-
-      default:
-        // eslint-disable-next-line typesafe/no-throw-sync-func
-        throw new Error(
-          `ApiVersion ${this.apiVersion} provided is invalid for function 'callback'`,
-        )
-    }
-  }
-
-  private async callbackV1(
-    code: string,
-    nonce: string | null = null,
-    redirectUri = this.getFirstRedirectUri(),
   ): Promise<{ sub: string; accessToken: string }> {
     const tokenSet = await this.sgID.callback(
       redirectUri,
@@ -255,32 +150,6 @@ export class SgidClient {
     }
     if (!accessToken) {
       throw new Error(Errors.NO_ACCESS_TOKEN_ERROR)
-    }
-    return { sub, accessToken }
-  }
-
-  private async callbackV2(
-    code: string,
-    nonce: string | null = null,
-    redirectUri = this.getFirstRedirectUri(),
-    codeVerifier?: string,
-  ): Promise<{ sub: string; accessToken: string }> {
-    if (codeVerifier === undefined) {
-      // eslint-disable-next-line typesafe/no-throw-sync-func
-      throw new Error(
-        "Code verifier must be provided in 'callback' when using apiVersion 2",
-      )
-    }
-
-    const tokenSet = await this.sgID.callback(
-      redirectUri,
-      { code },
-      { nonce: nonce ?? undefined, code_verifier: codeVerifier },
-    )
-    const { sub } = tokenSet.claims()
-    const { access_token: accessToken } = tokenSet
-    if (!sub || !accessToken) {
-      throw new Error('Missing sub claim or access token')
     }
     return { sub, accessToken }
   }
@@ -354,50 +223,6 @@ export class SgidClient {
       throw new Error(Errors.DECRYPT_PAYLOAD_ERROR)
     }
     return result
-  }
-
-  /**
-   * Generates a PKCE challenge pair where `codeChallenge` is the generated S256 challenge from `codeVerifier`
-   * @param length The length of the code verifier
-   * @returns The generated challenge pair
-   */
-  generatePkcePair(length = 43): {
-    codeVerifier: string
-    codeChallenge: string
-  } {
-    const codeVerifier = this.generateCodeVerifier(length)
-    const codeChallenge = this.generateCodeChallenge(codeVerifier)
-
-    return { codeVerifier, codeChallenge }
-  }
-
-  /**
-   * Generates the code verifier (random bytes encoded in url safe base 64) to be used in the OAuth 2.0 PKCE flow
-   * @param length The length of the code verifier to generate (Defaults to 43 if not provided)
-   * @returns The generated code verifier
-   */
-  generateCodeVerifier(length = 43): string {
-    if (length < 43 || length > 128) {
-      // eslint-disable-next-line typesafe/no-throw-sync-func
-      throw new Error(
-        `The code verifier should have a minimum length of 43 and a maximum length of 128. Length of ${length} was provided`,
-      )
-    }
-
-    // 96 bytes results in a 128 long base64 string
-    const codeVerifier = generators.codeVerifier(96)
-
-    // This works because a prefix of a random string is still random
-    return codeVerifier.slice(0, length)
-  }
-
-  /**
-   * Calculates the S256 PKCE code challenge for a provided code verifier
-   * @param codeVerifier The code verifier to calculate the S256 code challenge for
-   * @returns The calculated code challenge
-   */
-  generateCodeChallenge(codeVerifier: string): string {
-    return generators.codeChallenge(codeVerifier)
   }
 }
 
--- test/SgidClient.spec.ts
+++ test/SgidClient.spec.ts
@@ -1,27 +1,31 @@
 import { readFileSync } from 'fs'
 
-import SgidClient from '../src'
-import { codeVerifierAndChallengePattern } from '../src/util'
+import SgidClient, { DEFAULT_SCOPE } from '../src'
 
 import {
   MOCK_ACCESS_TOKEN,
   MOCK_API_VERSION,
+  MOCK_AUTH_CODE,
+  MOCK_AUTH_ENDPOINT,
   MOCK_CLIENT_ID,
   MOCK_CLIENT_PRIVATE_KEY,
   MOCK_CLIENT_SECRET,
-  MOCK_CODE_VERIFIER,
   MOCK_HOSTNAME,
   MOCK_REDIRECT_URI,
   MOCK_SUB,
   MOCK_USERINFO_PLAINTEXT,
 } from './mocks/constants'
 import {
+  tokenHandlerNoSub,
+  tokenHandlerNoToken,
   userInfoHandlerMalformedData,
   userInfoHandlerMalformedKey,
   userInfoHandlerNoData,
   userInfoHandlerNoKey,
 } from './mocks/handlers'
 import { server } from './mocks/server'
+
+const DEFAULT_RESPONSE_TYPE = 'code'
 
 describe('SgidClient', () => {
   let client: SgidClient
@@ -57,6 +61,213 @@ describe('SgidClient', () => {
       })
 
       expect(pkcs8Client).toBeDefined()
+    })
+  })
+
+  describe('authorizationUrl', () => {
+    it('should generate authorisation URL correctly when only state is provided', () => {
+      const mockState = 'mockState'
+
+      const { url, nonce } = client.authorizationUrl(mockState)
+
+      const actual = new URL(url)
+      // Count number of search params
+      let actualNumSearchParams = 0
+      actual.searchParams.forEach(() => actualNumSearchParams++)
+      const expected = new URL(MOCK_AUTH_ENDPOINT)
+      expect(actual.host).toBe(expected.host)
+      expect(actual.pathname).toBe(expected.pathname)
+      expect(actual.searchParams.get('client_id')).toBe(MOCK_CLIENT_ID)
+      expect(actual.searchParams.get('scope')).toBe(DEFAULT_SCOPE)
+      expect(actual.searchParams.get('response_type')).toBe(
+        DEFAULT_RESPONSE_TYPE,
+      )
+      expect(actual.searchParams.get('redirect_uri')).toBe(MOCK_REDIRECT_URI)
+      expect(actual.searchParams.get('nonce')).toBe(nonce)
+      expect(actual.searchParams.get('state')).toBe(mockState)
+      // Client ID, scope, response_type, redirect_uri, nonce, state
+      expect(actualNumSearchParams).toBe(6)
+    })
+
+    it('should generate authorisation URL correctly when scope is provided as a string', () => {
+      const mockState = 'mockState'
+      const mockScope = 'mockScope'
+
+      const { url, nonce } = client.authorizationUrl(mockState, mockScope)
+
+      const actual = new URL(url)
+      // Count number of search params
+      let actualNumSearchParams = 0
+      actual.searchParams.forEach(() => actualNumSearchParams++)
+      const expected = new URL(MOCK_AUTH_ENDPOINT)
+      expect(actual.host).toBe(expected.host)
+      expect(actual.pathname).toBe(expected.pathname)
+      expect(actual.searchParams.get('client_id')).toBe(MOCK_CLIENT_ID)
+      // Important check is here
+      expect(actual.searchParams.get('scope')).toBe(mockScope)
+      expect(actual.searchParams.get('response_type')).toBe(
+        DEFAULT_RESPONSE_TYPE,
+      )
+      expect(actual.searchParams.get('redirect_uri')).toBe(MOCK_REDIRECT_URI)
+      expect(actual.searchParams.get('nonce')).toBe(nonce)
+      expect(actual.searchParams.get('state')).toBe(mockState)
+      // Client ID, scope, response_type, redirect_uri, nonce, state
+      expect(actualNumSearchParams).toBe(6)
+    })
+
+    it('should generate authorisation URL correctly when scope is provided as a string array', () => {
+      const mockState = 'mockState'
+      const mockScopes = ['mockScope1', 'mockScope2', 'mockScope3']
+
+      const { url, nonce } = client.authorizationUrl(mockState, mockScopes)
+
+      const actual = new URL(url)
+      // Count number of search params
+      let actualNumSearchParams = 0
+      actual.searchParams.forEach(() => actualNumSearchParams++)
+      const expected = new URL(MOCK_AUTH_ENDPOINT)
+      expect(actual.host).toBe(expected.host)
+      expect(actual.pathname).toBe(expected.pathname)
+      expect(actual.searchParams.get('client_id')).toBe(MOCK_CLIENT_ID)
+      // Important check is here
+      expect(actual.searchParams.get('scope')).toBe(mockScopes.join(' '))
+      expect(actual.searchParams.get('response_type')).toBe(
+        DEFAULT_RESPONSE_TYPE,
+      )
+      expect(actual.searchParams.get('redirect_uri')).toBe(MOCK_REDIRECT_URI)
+      expect(actual.searchParams.get('nonce')).toBe(nonce)
+      expect(actual.searchParams.get('state')).toBe(mockState)
+      // Client ID, scope, response_type, redirect_uri, nonce, state
+      expect(actualNumSearchParams).toBe(6)
+    })
+
+    it('should generate authorisation URL correctly when nonce is specified', () => {
+      const mockState = 'mockState'
+      const mockNonce = 'mockNonce'
+
+      const { url, nonce } = client.authorizationUrl(
+        mockState,
+        undefined,
+        mockNonce,
+      )
+
+      const actual = new URL(url)
+      // Count number of search params
+      let actualNumSearchParams = 0
+      actual.searchParams.forEach(() => actualNumSearchParams++)
+      const expected = new URL(MOCK_AUTH_ENDPOINT)
+      expect(actual.host).toBe(expected.host)
+      expect(actual.pathname).toBe(expected.pathname)
+      expect(actual.searchParams.get('client_id')).toBe(MOCK_CLIENT_ID)
+      expect(actual.searchParams.get('scope')).toBe(DEFAULT_SCOPE)
+      expect(actual.searchParams.get('response_type')).toBe(
+        DEFAULT_RESPONSE_TYPE,
+      )
+      expect(actual.searchParams.get('redirect_uri')).toBe(MOCK_REDIRECT_URI)
+      // Important check is here
+      expect(actual.searchParams.get('nonce')).toBe(mockNonce)
+      expect(nonce).toBe(mockNonce)
+      expect(actual.searchParams.get('state')).toBe(mockState)
+      // Client ID, scope, response_type, redirect_uri, nonce, state
+      expect(actualNumSearchParams).toBe(6)
+    })
+
+    it('should generate authorisation URL correctly when nonce is null', () => {
+      const mockState = 'mockState'
+
+      const { url, nonce } = client.authorizationUrl(mockState, undefined, null)
+
+      const actual = new URL(url)
+      // Count number of search params
+      let actualNumSearchParams = 0
+      actual.searchParams.forEach(() => actualNumSearchParams++)
+      const expected = new URL(MOCK_AUTH_ENDPOINT)
+      expect(actual.host).toBe(expected.host)
+      expect(actual.pathname).toBe(expected.pathname)
+      expect(actual.searchParams.get('client_id')).toBe(MOCK_CLIENT_ID)
+      expect(actual.searchParams.get('scope')).toBe(DEFAULT_SCOPE)
+      expect(actual.searchParams.get('response_type')).toBe(
+        DEFAULT_RESPONSE_TYPE,
+      )
+      expect(actual.searchParams.get('redirect_uri')).toBe(MOCK_REDIRECT_URI)
+      // Important check is here
+      expect(actual.searchParams.get('nonce')).toBeNull()
+      expect(nonce).toBeUndefined()
+      expect(actual.searchParams.get('state')).toBe(mockState)
+      // Client ID, scope, response_type, redirect_uri, state
+      expect(actualNumSearchParams).toBe(5)
+    })
+
+    it('should generate authorisation URL correctly when redirectUri is provided', () => {
+      const mockState = 'mockState'
+      const mockRedirectUri = 'https://mockRedirectUri.com'
+
+      const { url, nonce } = client.authorizationUrl(
+        mockState,
+        undefined,
+        undefined,
+        mockRedirectUri,
+      )
+
+      const actual = new URL(url)
+      // Count number of search params
+      let actualNumSearchParams = 0
+      actual.searchParams.forEach(() => actualNumSearchParams++)
+      const expected = new URL(MOCK_AUTH_ENDPOINT)
+      expect(actual.host).toBe(expected.host)
+      expect(actual.pathname).toBe(expected.pathname)
+      expect(actual.searchParams.get('client_id')).toBe(MOCK_CLIENT_ID)
+      expect(actual.searchParams.get('scope')).toBe(DEFAULT_SCOPE)
+      expect(actual.searchParams.get('response_type')).toBe(
+        DEFAULT_RESPONSE_TYPE,
+      )
+      // Important check is here
+      expect(actual.searchParams.get('redirect_uri')).toBe(mockRedirectUri)
+      expect(actual.searchParams.get('nonce')).toBe(nonce)
+      expect(actual.searchParams.get('state')).toBe(mockState)
+      // Client ID, scope, response_type, redirect_uri, nonce, state
+      expect(actualNumSearchParams).toBe(6)
+    })
+
+    it('should throw when no redirectUri is provided', () => {
+      const mockState = 'mockState'
+
+      const noRedirectUriClient = new SgidClient({
+        clientId: MOCK_CLIENT_ID,
+        clientSecret: MOCK_CLIENT_SECRET,
+        privateKey: MOCK_CLIENT_PRIVATE_KEY,
+        hostname: MOCK_HOSTNAME,
+        apiVersion: MOCK_API_VERSION,
+      })
+
+      expect(() => noRedirectUriClient.authorizationUrl(mockState)).toThrow(
+        'No redirect URI registered with this client',
+      )
+    })
+  })
+
+  describe('callback', () => {
+    it('should call token endpoint and return sub and accessToken', async () => {
+      const { sub, accessToken } = await client.callback(MOCK_AUTH_CODE)
+
+      expect(sub).toBe(MOCK_SUB)
+      expect(accessToken).toBe(MOCK_ACCESS_TOKEN)
+    })
+
+    it('should throw when no access token is returned', async () => {
+      server.use(tokenHandlerNoToken)
+
+      await expect(client.callback(MOCK_AUTH_CODE)).rejects.toThrow(
+        'Missing sub claim or access token',
+      )
+    })
+
+    it('should throw when sub is empty', async () => {
+      server.use(tokenHandlerNoSub)
+
+      await expect(client.callback(MOCK_AUTH_CODE)).rejects.toThrow(
+        'Missing sub claim or access token',
+      )
     })
   })
 
@@ -115,55 +326,6 @@ describe('SgidClient', () => {
       await expect(client.userinfo(MOCK_ACCESS_TOKEN)).rejects.toThrow(
         'Unable to decrypt payload',
       )
-    })
-  })
-
-  describe('generatePkcePair', () => {
-    it('can generate a PKCE pair', () => {
-      const pkcePair = client.generatePkcePair()
-      expect(pkcePair).toBeDefined()
-    })
-  })
-
-  describe('generateCodeVerifier', () => {
-    it('should generate a code verifier of length 43 when no length is provided', () => {
-      const codeVerifier = client.generateCodeVerifier()
-
-      expect(codeVerifier.length).toBe(43)
-      expect(codeVerifier).toMatch(codeVerifierAndChallengePattern)
-    })
-
-    it('should generate a code verifier of specified length when length between 43 (inclusive) and 128 (inclusive) is provided', () => {
-      for (let length = 43; length <= 128; length++) {
-        const codeVerifier = client.generateCodeVerifier(length)
-        expect(codeVerifier.length).toBe(length)
-        expect(codeVerifier).toMatch(codeVerifierAndChallengePattern)
-      }
-    })
-
-    it('should throw an error when a length < 43 or length > 128 is provided', () => {
-      for (const length of [-1, 0, 42, 129, 138, 999]) {
-        expect(() => client.generateCodeVerifier(length)).toThrowError(
-          `The code verifier should have a minimum length of 43 and a maximum length of 128. Length of ${length} was provided`,
-        )
-      }
-    })
-  })
-
-  describe('generateCodeChallenge', () => {
-    it('should match the specified pattern', () => {
-      expect(client.generateCodeChallenge(MOCK_CODE_VERIFIER)).toMatch(
-        codeVerifierAndChallengePattern,
-      )
-    })
-
-    it('should be deterministic (return the same code challenge given the same code verifier)', () => {
-      const firstCodeChallenge =
-        client.generateCodeChallenge(MOCK_CODE_VERIFIER)
-      const secondCodeChallenge =
-        client.generateCodeChallenge(MOCK_CODE_VERIFIER)
-
-      expect(firstCodeChallenge).toBe(secondCodeChallenge)
     })
   })
 })
