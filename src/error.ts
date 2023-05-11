72447a9810176cb0aa342448eab0f3bfca2b5421
raynerljm <raynerljm@gmail.com>
2023-05-11 15:38:40 +0800

    refactor: rename code verifier error

--- src/error.ts
+++ src/error.ts
@@ -1,6 +1,6 @@
 // PKCE generatePkcePair and generateCodeVerifier errors
 export const CODE_VERIFIER_LENGTH_ERROR =
-  'The code verifier should have a minimum length of 43 and a maximum length of 128'
+  'Code verifier should have a minimum length of 43 and a maximum length of 128'
 export const PKCE_PAIR_LENGTH_ERROR =
   'The function generatePkcePair should receive a minimum length of 43 and a maximum length of 128'
 
