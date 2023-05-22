a1d2f3f015a5402dbf6ac928031294e9476f702c
raynerljm <raynerljm@gmail.com>
2023-05-22 10:35:28 +0800

    feat: improve store to be global, change README to start dev instead of start

--- examples/nextjs-csr/src/lib/sgidClient.ts
+++ examples/nextjs-csr/src/lib/sgidClient.ts
@@ -1,13 +1,10 @@
-import { SgidClient } from "@opengovsg/sgid-client";
+import { SgidClient } from '@opengovsg/sgid-client'
 
 const sgidClient = new SgidClient({
-  clientId: String(process.env.SGID_CLIENT_ID) || "",
-  clientSecret: String(process.env.SGID_CLIENT_SECRET) || "",
-  privateKey: String(process.env.SGID_PRIVATE_KEY).replace(/\\n/g, "\n") || "",
-  redirectUri:
-    process.env.NEXT_ENV !== "development"
-      ? "https://sgid-nextjs-csr-demo.vercel.app/api/callback"
-      : "http://localhost:3000/api/callback",
-});
+  clientId: String(process.env.SGID_CLIENT_ID),
+  clientSecret: String(process.env.SGID_CLIENT_SECRET),
+  privateKey: String(process.env.SGID_PRIVATE_KEY),
+  redirectUri: 'http://localhost:3000/api/callback',
+})
 
-export { sgidClient };
+export { sgidClient }
--- examples/nextjs-csr/src/lib/store.ts
+++ examples/nextjs-csr/src/lib/store.ts
@@ -1,3 +1,13 @@
+type Global = typeof global
+
+interface GlobalWithStore extends Global {
+  store: Map<string, Session>
+}
+
+/**
+ * We place the store in the global object to prevent it from being cleared whenever compilation happens
+ * https://stackoverflow.com/questions/75272877/how-to-prevent-next-js-from-instantiating-a-singleton-class-object-multiple-time
+ */
 type Session = {
   state?: string
   codeVerifier?: string
@@ -7,6 +17,16 @@ type Session = {
   userInfo?: Record<string, string>
 }
 
-const store = new Map<string, Session>()
+let store: Map<string, Session>
+
+if (process.env.NODE_ENV === 'production') {
+  store = new Map<string, Session>()
+} else {
+  // If the store does not exist, initialize it
+  if (!(global as GlobalWithStore).store) {
+    ;(global as GlobalWithStore).store = new Map<string, Session>()
+  }
+  store = (global as GlobalWithStore).store
+}
 
 export { store }
