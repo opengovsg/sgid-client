48fd6ed91c810c5411d9420e23b98ec130be1471
Kwa Jie Hao <jie_hao_kwa@alumni.brown.edu>
2023-05-25 18:20:20 +0800

    chore: update nextjs ssr example to use port 5001

--- examples/nextjs-ssr/src/lib/sgidClient.ts
+++ examples/nextjs-ssr/src/lib/sgidClient.ts
@@ -4,7 +4,7 @@ const sgidClient = new SgidClient({
   clientId: String(process.env.SGID_CLIENT_ID),
   clientSecret: String(process.env.SGID_CLIENT_SECRET),
   privateKey: String(process.env.SGID_PRIVATE_KEY),
-  redirectUri: 'http://localhost:3000/success',
+  redirectUri: 'http://localhost:5001/success',
 })
 
 export { sgidClient }
